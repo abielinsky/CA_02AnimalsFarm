49d35dab369545844cd16e95783cc44ddb800622
Abiel <d00251844@student.dkit.ie>
2022-11-24 23:58:03 +0000

    Initial commit

---
+++ CA02_FarmAnimals/src/IMilkable.java
@@ -0,0 +1,22 @@
+public interface IMilkable {
+
+
+//    void moveTo(int x,int y);          // all methods are public by default
+//
+//    void draw();
+//
+//    double area();
+//
+//
+//
+//    int getX();
+//
+//    void setX(int x);
+//
+//    int getY();
+//
+//    void setY(int y);
+
+
+
+}
