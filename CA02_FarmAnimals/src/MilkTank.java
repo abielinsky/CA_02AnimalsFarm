81e16a1796a4498532b76f8c7d27c36facbe58b1
Abiel <d00251844@student.dkit.ie>
2022-11-25 11:24:01 +0000

    someCode
    MilkTank and Shed

--- CA02_FarmAnimals/src/MilkTank.java
+++ CA02_FarmAnimals/src/MilkTank.java
@@ -1,50 +1,154 @@
 public class MilkTank {
 
 
+
+    public static final int STANDARD_TANK_CAPACITY = 2000;
+    private String name;
     private double capacity;
-    private double currentAmount;
+    private double amountInTank;
 
-    public MilkTank(int capacity) {
+    MilkTank() {
+        this.name ="unnamed tank";
+        this.capacity = STANDARD_TANK_CAPACITY;
+        this.amountInTank=0;
+    }
+
+    MilkTank(String name, int capacity) {
+        this.name=name;
         this.capacity = capacity;
+        this.amountInTank=0;
     }
 
-    public MilkTank() {
-        this.capacity = 2000;
+    public String getName()
+    {
+        return this.name;
     }
 
-    public double getCapacity() {
-        return capacity;
+    public double getCapacity()
+    {
+        return this.capacity;
     }
 
-    public double freeSpace(){
-        return currentAmount - capacity;
+    public double getAmountInTank()
+    {
+        return amountInTank;
     }
 
-    public void addToTank(double amount){
-
-
+    public double freeSpace() {
+        return this.capacity - this.amountInTank;
     }
 
+    /**
+     * Extract/Remove milk from this tank.
+     * @param amountRequested
+     * @return amount of milk removed from the tank.
+     * Note, there may not be enough milk in the tank to satisfy the requested amount.
+     */
+    public double removeFromMilkTank(double amountRequested ) {
+        double amountRemoved=amountRequested;
 
-    public double getFromTank(double amount){
-    if (currentAmount >= capacity){
-        currentAmount -= amount;
-        return amount;
-
-    }else {
-        System.out.println("Empty Tank");
+        if( this.amountInTank >= amountRequested ){
+            amountInTank -= amountRequested;
+        }
+        else {   // there is not enough milk in the tank to satisfy the amount requested
+            // so, we remove what is available in the tank
+            amountRemoved = this.amountInTank;
+            this.amountInTank = 0.0;
+        }
+        return amountRemoved;
     }
 
-    return 0.00;
+    /**
+     * Add milk to this tank.
+     * @param amountPresented amount of milk in litres that we want to add to tank
+     * @return amountAdded - the amount actually added to the tank
+     */
+    public double addMilkToTank( double amountPresented ){
+
+        double amountAdded = amountPresented;
+
+        if(amountInTank + amountPresented > capacity)  { // amount will overfill the tank!
+            amountAdded = capacity - amountInTank;
+            amountInTank = capacity;  // set tank as full
+
+            //TODO - should we raise a warning if the amount is greater than the
+            // remaining space in the tank.  Discuss!
+            // As this code currently stands, if tank is filled beyond capacity
+            // the overflow is just ignored (dumped).
+        }
+        else {  // case where the tank can take the full amount being added
+            amountInTank += amountPresented;
+        }
+        return amountAdded;
     }
 
     @Override
     public String toString() {
         return "MilkTank{" +
-                "capacity=" + capacity +
-                ", currentAmount=" + currentAmount +
+                "name='" + name + '\'' +
+                ", capacity=" + capacity +
+                ", amountInTank=" + amountInTank +
                 '}';
     }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//
+//    public void addToTank(double amount){
+//        if (amount <= freeSpace()){
+//            currentAmount = currentAmount + amount;
+//        }
+//        else
+//        {
+//            System.out.println("Tank is Full...!!!");
+//        }
+//
+//    }
+//
+//
+//    public double getFromTank(double amount){
+//    if (currentAmount >= capacity){
+//        currentAmount -= amount;
+//        return amount;
+//
+//    }else {
+//        System.out.println("Empty Tank");
+//    }
+//
+//    return 0.00;
+//    }
+//
+//    @Override
+//    public String toString() {
+//        return "MilkTank{" +
+//                "capacity=" + capacity +
+//                ", currentAmount=" + currentAmount +
+//                '}';
+//    }
 
 
 }
---
+++ CA02_FarmAnimals/src/MilkTankApp.java
@@ -0,0 +1,80 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.InputMismatchException;
+import java.util.List;
+import java.util.Scanner;
+
+/**
+ * Demonstrates a suggested MilkTank class which forms part of your Farm Simulator Project.
+ * Demonstrates - reading MilkTank description records form a text file (where each line (record)
+ * in the text file represents the details of one Milk Talk).
+ * Each record is read, its values are extracted and used as input to a MilkTank constructor
+ * to create MilkTank objects. Each object is added to a LIst of MilkTanks.
+ */
+public class MilkTankApp {
+
+    public static void main(String[] args) {
+        MilkTankApp app = new MilkTankApp();
+        app.start();
+    }
+    public void start( ){
+
+        ArrayList<MilkTank> tankList = new ArrayList<>();
+
+        loadMilkTanks(tankList,"milktanks.txt");    // see text file in project pane
+        displayMilkTanks(tankList);
+
+        System.out.println("Test the MilkTank methods (best done in JUnit)");
+        MilkTank tank = tankList.get(0);    // access the first MilkTank
+
+        tank.addMilkToTank(100);
+        double amountRemoved = tank.removeFromMilkTank(150);
+        System.out.println("Amount removed from tank = " + amountRemoved);
+
+        tank.addMilkToTank(100);        // should create JUnit tests for this
+        System.out.println(tank.toString());
+    }
+
+    public static void displayMilkTanks(List<MilkTank> list) {
+        for (MilkTank tank : list) {
+            System.out.println(tank);
+        }
+    }
+
+    /**
+     * Read MilkTank records from a text file, create MilkTank objects
+     * and populate a List of MilkTank objects.
+     *
+     * @param tankList a MilkTank list to populate
+     * @param fileName name of text file as String
+     */
+    public static void loadMilkTanks(List<MilkTank> tankList, String fileName) {
+
+        System.out.println("In loadMilkTanks() - Loading Milk Tank data records from the text file");
+
+        File inputFile = new File(fileName);
+
+        try (Scanner scanner = new Scanner(inputFile))  // try-with-resources style
+        {
+            scanner.useDelimiter("[,\r\n]+");     // set comma and newline as the delimiters
+
+            while (scanner.hasNext())
+            {
+                String name = scanner.next();        // read name as a String
+                int capacity = scanner.nextInt();    // read capacity as an int
+
+                // to read a double value use scanner.nextDouble()
+
+                MilkTank tank = new MilkTank(name, capacity);
+
+                tankList.add(tank);
+            }
+
+        } catch (FileNotFoundException exception) {
+            System.out.println("FileNotFoundException caught." + exception);
+        } catch (InputMismatchException exception) {
+            System.out.println("InputMismatchException caught." + exception);
+        }
+    }
+}
