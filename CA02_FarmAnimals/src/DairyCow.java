10856f712b6f979ce89b4ec8ce2e6f2dcbacf74b
Abiel <d00251844@student.dkit.ie>
2022-11-28 09:32:59 +0000

    1
    1

--- CA02_FarmAnimals/src/DairyCow.java
+++ CA02_FarmAnimals/src/DairyCow.java
@@ -1,2 +1,25 @@
-public class DairyCow {
+public class DairyCow extends Animal implements IMilkable{
+
+    private int MIN_CAPACITY = 20;
+    private int MAX_CAPACITY = 40;
+
+    int udderCapacity;
+
+    DairyCow(int id){
+        super(id); //calls animal constructor
+        this.udderCapacity = Random(MIN_CAPACITY, MAX_CAPACITY);
+    }
+
+
+    public double milk()
+    {
+        //code to milk an animal
+        return Random (0, udderCapacity);
+    }
+
+
+
+
+
 }
+
--- CA02_FarmAnimals/src/IMilkable.java
+++ CA02_FarmAnimals/src/IMilkable.java
@@ -1,22 +1,6 @@
 public interface IMilkable {
 
-
-//    void moveTo(int x,int y);          // all methods are public by default
-//
-//    void draw();
-//
-//    double area();
-//
-//
-//
-//    int getX();
-//
-//    void setX(int x);
-//
-//    int getY();
-//
-//    void setY(int y);
-
+void milk ();
 
 
 }
