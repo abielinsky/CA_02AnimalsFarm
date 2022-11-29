c6782d1037e8fde6012edb3426b7c6644bddf0f1
Abiel <d00251844@student.dkit.ie>
2022-11-29 00:24:33 +0000

    DailyCow

--- CA02_FarmAnimals/src/AppSimulator.java
+++ CA02_FarmAnimals/src/AppSimulator.java
@@ -2,24 +2,24 @@ import java.util.ArrayList;
 
 public class AppSimulator {
 
-    public static void main(String[] args) {
-
-        ArrayList<Animal> animalList  = new ArrayLis <>();
-        animalList.add( new DairyCow(101));
-
-        //double amount = animalsList.get(0).milk(); //Milk a Dairy Cow;
-
-        BeefCow beefCow1 = new BeefCow(501));
-        beefCow.milk();  ///////////////
-
-
-        // milk all the animals ??
-        for(Animal animal : animalList){
-           if (animal instanceof IMilkable) {
-               int amount = animal.milk();
-               totalMilk += amount;
-           }
-        }
+//    public static void main(String[] args) {
+//
+//        ArrayList<Animal> animalList  = new ArrayLis <>();
+//        animalList.add( new DairyCow(101));
+//
+//        //double amount = animalsList.get(0).milk(); //Milk a Dairy Cow;
+//
+//        BeefCow beefCow1 = new BeefCow(501));
+//        beefCow.milk();  ///////////////
+//
+//
+//        // milk all the animals ??
+//        for(Animal animal : animalList){
+//           if (animal instanceof IMilkable) {
+//               int amount = animal.milk();
+//               totalMilk += amount;
+//           }
+//        }
 
 
 
